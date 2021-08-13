8dfd5aa004e1e6c1f346900aa54d54e5ca87bd51
Nori <loveandfight2186@yahoo.co.jp>
2021-08-13 13:35:22 -0700

    delete action's and push import

--- src/templates/Login.jsx
+++ src/templates/Login.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import { useDispatch } from "react-redux";
-import { push } from "connected-react-router";
-import { signInAction } from "../reducks/users/actions";
 import { signIn } from "../reducks/users/operations";
 
 export default function Login() {
