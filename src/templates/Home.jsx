a33111fb8f7d5d3e6955d67710760947db98327b
Nori <loveandfight2186@yahoo.co.jp>
2021-08-12 19:53:46 -0700

    create user selectors file

--- src/templates/Home.jsx
+++ src/templates/Home.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import { useSelector } from "react-redux";
+import { getUserId } from "../reducks/users/selectors";
 
 export default function Home() {
+  const selector = useSelector((state) => state);
+  const uid = getUserId(selector);
+
   return (
-    <h2>
-      HOME
-    </h2>
-  )
+    <>
+      <h2>HOME</h2>
+      <p>{uid}</p>
+    </>
+  );
 }
--- src/templates/Login.jsx
+++ src/templates/Login.jsx
@@ -1,16 +1,21 @@
-import React from 'react'
+import React from "react";
 import { useDispatch } from "react-redux";
 import { push } from "connected-react-router";
-
+import { signInAction } from "../reducks/users/actions";
 
 export default function Login() {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   return (
     <div>
       <h2>Login</h2>
-      <button onClick={() => dispatch(push('/'))}>
+      <button
+        onClick={() => {
+          dispatch(signInAction({ uid: "0001", username: "nori" }));
+          dispatch(push("/"));
+        }}
+      >
         Login
       </button>
     </div>
-  )
+  );
 }
