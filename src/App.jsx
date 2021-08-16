0009150a6afc6b303d13f109be31e868edc61c44
Nori <loveandfight2186@yahoo.co.jp>
2021-08-16 00:01:57 -0700

    implement signup form

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import Router from './Router'
+import React from "react";
+import Router from "./Router";
+import "./assets/reset.css";
+import "./assets/style.css";
 
 export default function App() {
   return (
     <main>
       <Router />
     </main>
-  )
+  );
 }
--- src/Router.jsx
+++ src/Router.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import { Route, Switch } from 'react-router'
-import {Login, Home} from './templates'
+import React from "react";
+import { Route, Switch } from "react-router";
+import { Login, Home, Signup } from "./templates";
+import SignUp from "./templates/SignUp";
 
 export default function Router() {
   return (
     <Switch>
-      <Route exact path={"/login"} component={Login}/>
-      <Route exact path={"(/)?"} component={Home}/>
+      <Route exact path={"/login"} component={Login} />
+      <Route exact path={"(/)?"} component={Home} />
+      <Route exact path={"/signup"} component={SignUp} />
     </Switch>
-  )
+  );
 }
