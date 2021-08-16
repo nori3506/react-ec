0b0bd01ce66b0c04f4d635474166a4ed5a560275
Nori <loveandfight2186@yahoo.co.jp>
2021-08-15 23:23:37 -0700

    create css files

---
+++ src/firebase/config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+  apiKey: "AIzaSyCpUOTWPYZWB--ex3NocnT1E538WO6isLU",
+  authDomain: "react-ec-1781b.firebaseapp.com",
+  projectId: "react-ec-1781b",
+  storageBucket: "react-ec-1781b.appspot.com",
+  messagingSenderId: "397071055154",
+  appId: "1:397071055154:web:cedc6b52d5c63a3c34bf2b",
+  measurementId: "G-DCB51BL9KN",
+};
