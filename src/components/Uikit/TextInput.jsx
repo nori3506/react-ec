5d3ee3b9ea6d6adadc703f73798cddc1a3c91cbd
Nori <loveandfight2186@yahoo.co.jp>
2021-08-15 23:30:45 -0700

    implement: Textinput.jsx

---
+++ src/components/Uikit/TextInput.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { TextField } from "@material-ui/core";
+
+export default function TextInput(props) {
+  return (
+    <TextField
+      fullWidth={props.fullWidth}
+      label={props.label}
+      margin="dense"
+      multiline={props.multiline}
+      required={props.required}
+      rows={props.rows}
+      falue={props.value}
+      type={props.type}
+      onChange={props.onChange}
+    />
+  );
+}
