1a2967de471927a7094092b518ec8fe70df564c6
Nori <loveandfight2186@yahoo.co.jp>
2021-08-19 16:22:17 -0700

    create productList.jsx

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 import { Route, Switch } from "react-router";
 import Auth from "./Auth";
-import { SignIn, Home, SignUp, Reset, ProductEdit } from "./templates";
+import {
+  SignIn,
+  Home,
+  SignUp,
+  Reset,
+  ProductEdit,
+  ProductList,
+} from "./templates";
 
 export default function Router() {
   return (
@@ -10,7 +17,7 @@ export default function Router() {
       <Route exact path={"/signup"} component={SignUp} />
       <Route exact path={"/signin/reset"} component={Reset} />
       <Auth>
-        <Route exact path={"(/)?"} component={Home} />
+        <Route exact path={"(/)?"} component={ProductList} />
         <Route path={"/product/edit(/:id)?"} component={ProductEdit} />
       </Auth>
     </Switch>
