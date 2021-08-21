f75c335a01ff9791352f11478966b701777e7e77
Nori <loveandfight2186@yahoo.co.jp>
2021-08-20 17:39:16 -0700

    add logo and link to home

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,7 +1,49 @@
 import React from "react";
+import { AppBar, Toolbar } from "@material-ui/core";
+import Logo from "../../assets/img/logo.png";
+import { makeStyles } from "@material-ui/styles";
+import { useDispatch, useSelector } from "react-redux";
+import { getIsSignedIn } from "../../reducks/users/selectors";
+import { push } from "connected-react-router";
 
-const header = () => {
-  return <div></div>;
+const useStyles = makeStyles({
+  root: {
+    flexGrow: 1,
+  },
+  menuBar: {
+    backgroundColor: "#fff",
+    color: "#444",
+  },
+  toolBar: {
+    margin: "0 auto",
+    maxWidth: 1024,
+    width: 100,
+  },
+  iconButtons: {
+    margin: "0 0 0 auto",
+  },
+});
+
+const Header = () => {
+  const classes = useStyles();
+  const selector = useSelector((state) => state);
+  const isSignedIn = getIsSignedIn(selector);
+  const dispatch = useDispatch();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="fixed" className={classes.menuBar}>
+        <Toolbar className={classes.toolBar}>
+          <img
+            src={Logo}
+            alt="Site Logo"
+            width="128px"
+            onClick={() => dispatch(push("/"))}
+          />
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 };
 
-export default header;
+export default Header;
