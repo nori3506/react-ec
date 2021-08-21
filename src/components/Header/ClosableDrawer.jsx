f39c9e97025112b8939c3d53de86c2e47333dc23
Nori <loveandfight2186@yahoo.co.jp>
2021-08-21 13:08:42 -0700

    func: create drawer in header

---
+++ src/components/Header/ClosableDrawer.jsx
@@ -0,0 +1,136 @@
+import React, { useState, useCallback } from "react";
+import IconButton from "@material-ui/core/IconButton";
+import Drawer from "@material-ui/core/Drawer";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import { makeStyles, createStyles } from "@material-ui/core/styles";
+import { Divider } from "@material-ui/core";
+import SearchIcon from "@material-ui/icons/Search";
+import HistoryIcon from "@material-ui/icons/History";
+import PersonIcon from "@material-ui/icons/Person";
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
+import { TextInput } from "../Uikit";
+import { push } from "connected-react-router";
+import { useDispatch } from "react-redux";
+import AddCircleIcon from "@material-ui/icons/AddCircle";
+import { signOut } from "../../reducks/users/operations";
+
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    drawer: {
+      [theme.breakpoints.up("sm")]: {
+        width: 256,
+        flexShrink: 0,
+      },
+    },
+    // necessary for content to be below app bar
+    toolbar: theme.mixins.toolbar,
+    drawerPaper: {
+      width: 256,
+    },
+    searchField: {
+      alignItems: "center",
+      display: "flex",
+      marginLeft: 32,
+    },
+  })
+);
+
+const ClosableDrawer = (props) => {
+  const classes = useStyles();
+  const { container } = props;
+  const [keyword, setKeyword] = useState("");
+  const dispatch = useDispatch();
+
+  const inputKeyword = useCallback(
+    (event) => {
+      setKeyword(event.target.value);
+    },
+    [setKeyword]
+  );
+
+  const selectMenu = (event, path) => {
+    dispatch(push(path));
+    props.onClose(event, false);
+  };
+
+  const menus = [
+    {
+      func: selectMenu,
+      label: "Product Register",
+      icon: <AddCircleIcon />,
+      id: "register",
+      value: "/product/edit",
+    },
+    {
+      func: selectMenu,
+      label: "Your Order",
+      icon: <HistoryIcon />,
+      id: "history",
+      value: "/",
+    },
+    {
+      func: selectMenu,
+      label: "Profile",
+      icon: <PersonIcon />,
+      id: "profile",
+      value: "/user/mypage",
+    },
+  ];
+
+  return (
+    <nav className={classes.drawer}>
+      <Drawer
+        container={container}
+        variant="temporary"
+        anchor={"right"}
+        open={props.open}
+        onClose={(e) => props.onClose(e, false)}
+        classes={{ paper: classes.drawerPaper }}
+        ModalProps={{ keepMounted: true }}
+      >
+        <div className="">
+          <div className={classes.searchField}>
+            <TextInput
+              fullWidth={false}
+              label={"Kyeword"}
+              multiline={false}
+              onChange={inputKeyword}
+              require={false}
+              rows={1}
+              value={keyword}
+              type={"text"}
+            />
+            <IconButton>
+              <SearchIcon />
+            </IconButton>
+          </div>
+          <Divider />
+          <List>
+            {menus.map((menu) => (
+              <ListItem
+                button
+                key={menu.id}
+                onClick={(e) => menu.func(e, menu.value)}
+              >
+                <ListItemIcon>{menu.icon}</ListItemIcon>
+                <ListItemText primary={menu.label} />
+              </ListItem>
+            ))}
+            <ListItem button key="logout" onClick={() => dispatch(signOut())}>
+              <ListItemIcon>
+                <ExitToAppIcon />
+              </ListItemIcon>
+              <ListItemText primary="Logout" />
+            </ListItem>
+          </List>
+          <Divider />
+        </div>
+      </Drawer>
+    </nav>
+  );
+};
+
+export default ClosableDrawer;
--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,11 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { AppBar, Toolbar } from "@material-ui/core";
 import Logo from "../../assets/img/logo.png";
 import { makeStyles } from "@material-ui/styles";
 import { useDispatch, useSelector } from "react-redux";
 import { getIsSignedIn } from "../../reducks/users/selectors";
 import { push } from "connected-react-router";
-import HeaderMenus from "./HeaderMenus";
+import { ClosableDrawer, HeaderMenus } from "./index";
+import { useCallback } from "react";
 
 const useStyles = makeStyles({
   root: {
@@ -30,7 +31,19 @@ const Header = () => {
   const selector = useSelector((state) => state);
   const isSignedIn = getIsSignedIn(selector);
   const dispatch = useDispatch();
-
+  const [open, setOpen] = useState(false);
+  const handleDrawerToggle = useCallback(
+    (event) => {
+      if (
+        event.type === "keydown" &&
+        (event.key === "Tab" || event.key === "Shift")
+      ) {
+        return;
+      }
+      setOpen(!open);
+    },
+    [setOpen, open]
+  );
   return (
     <div className={classes.root}>
       <AppBar position="fixed" className={classes.menuBar}>
@@ -43,11 +56,12 @@ const Header = () => {
           />
           {isSignedIn && (
             <div className={classes.iconButtons}>
-              <HeaderMenus />
+              <HeaderMenus handleDrawerToggle={handleDrawerToggle} />
             </div>
           )}
         </Toolbar>
       </AppBar>
+      <ClosableDrawer open={open} onClose={handleDrawerToggle} />
     </div>
   );
 };
