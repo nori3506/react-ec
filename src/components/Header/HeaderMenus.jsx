3d2d30704c687e9268525833c9619353b9376fd8
Nori <loveandfight2186@yahoo.co.jp>
2021-08-21 14:33:49 -0700

    create productInCart method

--- src/components/Header/HeaderMenus.jsx
+++ src/components/Header/HeaderMenus.jsx
@@ -3,12 +3,16 @@ import React from "react";
 import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
 import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
 import MenuIcon from "@material-ui/icons/Menu";
+import { getProductsInCart } from "../../reducks/users/selectors";
+import { useSelector } from "react-redux";
 
 const HeaderMenus = (props) => {
+  const selector = useSelector((state) => state);
+  let productsInCart = getProductsInCart(selector);
   return (
     <>
       <IconButton>
-        <Badge badgeContent={3} color="secondary">
+        <Badge badgeContent={productsInCart.length} color="secondary">
           <ShoppingCartIcon />
         </Badge>
       </IconButton>
--- src/reducks/store/initialState.js
+++ src/reducks/store/initialState.js
@@ -1,6 +1,7 @@
 const initialState = {
   products: { list: [] },
   users: {
+    cart: [],
     isSignedIn: false,
     role: "",
     uid: "",
