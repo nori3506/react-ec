5424ae76446962ec6a8ab6f519d897ef15056b60
Nori <loveandfight2186@yahoo.co.jp>
2021-08-23 15:53:46 -0700

    create OrderHistoryItem.jsx

---
+++ src/components/Products/OrderHistoryItem.jsx
@@ -0,0 +1,9 @@
+import Divider from "@material-ui/core/Divider";
+import React from "react";
+import { TextDetails } from "../Uikit";
+
+const OrderHistoryItem = (props) => {
+  return <div></div>;
+};
+
+export default OrderHistoryItem;
