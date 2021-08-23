4eb6a003d5d4df12ad6cb6f8373bc1b4728944dc
Nori <loveandfight2186@yahoo.co.jp>
2021-08-23 00:39:14 -0700

    import files in OrderConfirm.jsx

--- src/templates/OrderConfirm.jsx
+++ src/templates/OrderConfirm.jsx
@@ -1,4 +1,12 @@
-import React from "react";
+import React, { useCallback, useMemo } from "react";
+import React, { useCallback, useMemo } from "react";
+import { makeStyles } from "@material-ui/styles";
+import { useDispatch, useSelector } from "react-redux";
+import { CartListItem } from "../components/Products";
+import { getProductsInCart } from "../reducks/users/selectors";
+import { List } from "@material-ui/core/List";
+import { Divider } from "@material-ui/core/Divider";
+import { PrimaryButton } from "../components/Uikit";
 
 const OrderConfirm = () => {
   return <div></div>;
